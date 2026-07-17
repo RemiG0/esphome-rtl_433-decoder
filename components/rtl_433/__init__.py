a53e8c33da0761decfedc8c31ae8324fd6a6f0f6
RemiG0 <66305257+RemiG0@users.noreply.github.com>
2026-07-17 15:43:53 +0200

    Fix add_library call with None parameter

--- components/rtl_433/__init__.py
+++ components/rtl_433/__init__.py
@@ -40,7 +40,7 @@ async def to_code(config):
     await cg.register_component(var, config)
     
     #cg.add_library("https://github.com/juanboro/rtl_433_Decoder_ESP.git",None)
-    cg.add_library("https://github.com/NorthernMan54/rtl_433_ESP.git")
+    cg.add_library("https://github.com/NorthernMan54/rtl_433_ESP.git",None)
     if remote_base.CONF_RECEIVER_ID in config:
         await remote_base.register_listener(var, config)
 
